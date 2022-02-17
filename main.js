67590b8caf1d92ad5e7cdf66b7bbdfbf89f34e2d
bapple0531 <98273676+bapple0531@users.noreply.github.com>
2022-02-17 17:27:51 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+//variable representing my age
+const myAge = 29;
+
+//setting a variable for the amount of early years
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5
+
+//subtracting two because we've accounted for the first two years
+let laterYears = myAge - 2;
+
+//no. of dog years accounted for in later years
+laterYears *= 4;
+
+//Adding all the years to get my age in dog years
+myAgeInDogYears = earlyYears + laterYears;
+
+//creating a string and converting it to lowercase
+const myName = 'Ben' .toLowerCase();
+
+//logging to the console my work
+console.log(`My name is ${myName}. I am ${myAge} years old
+in human years which is ${myAgeInDogYears} years old in dog 
+years.`);
+ 
